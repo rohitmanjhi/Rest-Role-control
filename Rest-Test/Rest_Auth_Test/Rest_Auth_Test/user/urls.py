a7c307416158c03cc0ed6e7a232c99109c0b2e79
rohit <rohit.manjhi@djtcorp.in>
2020-10-07 20:28:38 +0530

    some changes in urls

--- Rest-Test/Rest_Auth_Test/Rest_Auth_Test/user/urls.py
+++ Rest-Test/Rest_Auth_Test/Rest_Auth_Test/user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path, re_path
-from .views import SignUp, ForgotPassword, user_detail, user_list, user_add, Login
+from .views import SignUp, ForgotPassword, Login
 
 urlpatterns = [
     path('api/signup/', SignUp.as_view(),
          name='sign_up'),
     path('api/forgot/password', ForgotPassword.as_view(),
          name='forgot_password'),
-    path('token/', Login.as_view(), name='token_obtain_pair'),
+    path('api/login/', Login.as_view(), name='token_obtain_pair'),
 ]
